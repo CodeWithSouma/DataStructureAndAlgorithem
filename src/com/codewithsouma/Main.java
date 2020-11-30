5a52b25ee51c509d3f3f5753363cd06450fc0330
Souma <souma.hitech@gmail.com>
2020-12-01 02:09:31 +0530

    Create queue using stack

--- src/com/codewithsouma/Main.java
+++ src/com/codewithsouma/Main.java
@@ -1,37 +1,29 @@
 package com.codewithsouma;
 
 
-import com.codewithsouma.queue.ArrayQueue;
-
-import java.util.ArrayDeque;
-import java.util.Queue;
+import com.codewithsouma.queue.QueueWithTwoStack;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayQueue queue = new ArrayQueue(3);
+        QueueWithTwoStack queue = new QueueWithTwoStack();
         queue.enqueue(10);
         queue.enqueue(20);
         queue.enqueue(30);
+        queue.enqueue(40);
+
         System.out.println(queue.peek());
         System.out.println(queue.dequeue());
         System.out.println(queue.dequeue());
         System.out.println(queue.dequeue());
-        System.out.println(queue.isEmpty());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
 
-        System.out.println(queue);
+
+
 
     }
 
-/*    public static void reverse(Queue<Integer> queue){
-        if (queue.isEmpty()) return;
 
-        Stack<Integer> stack = new Stack<>();
-        while (!queue.isEmpty())
-            stack.add(queue.remove());
-
-        while (!stack.isEmpty())
-            queue.add(stack.pop());
-    }*/
 }
