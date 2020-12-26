776478387027507dcd610e2d55a1c7d9c035565f
Souma <souma.hitech@gmail.com>
2020-12-26 19:28:35 +0530

    add equals method for check two tree are equals or not

--- src/com/codewithsouma/trees/Tree.java
+++ src/com/codewithsouma/trees/Tree.java
@@ -1,5 +1,7 @@
 package com.codewithsouma.trees;
 
+import com.sun.source.tree.ClassTree;
+
 public class Tree {
     private class Node {
         private int value;
@@ -139,6 +141,21 @@ public class Tree {
         }
 
         return last.value;
+    }
+
+    public boolean equals(Tree other){
+        if (other == null) return false;
+        return equals(root,other.root);
+    }
+
+    private boolean equals(Node first, Node second){
+        if (first == null && second == null) return true;
+
+        if (first != null && second != null)
+            return first.value == second.value && equals(first.leftChild,second.leftChild)
+                    && equals(first.rightChild , second.rightChild);
+
+        return false;
     }
 
 
