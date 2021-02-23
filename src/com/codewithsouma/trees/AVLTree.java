560a0f6dcc7ff358a1b0c25311c52affd86a10da
Souma <souma.hitech@gmail.com>
2021-02-23 21:44:54 +0530

    add balance factor in AVLTree

--- src/com/codewithsouma/trees/AVLTree.java
+++ src/com/codewithsouma/trees/AVLTree.java
@@ -1,7 +1,8 @@
 package com.codewithsouma.trees;
 
 public class AVLTree {
-    private class AVLNode{
+    private class AVLNode {
+        private int height;
         private int value;
         private AVLNode leftChild;
         private AVLNode rightChild;
@@ -20,20 +21,43 @@ public class AVLTree {
 
     private AVLNode root;
 
-    public void insert(int value){
-        root = insert(root,value);
+    public void insert(int value) {
+        root = insert(root, value);
     }
 
-    private AVLNode insert(AVLNode root, int value){
+    private AVLNode insert(AVLNode root, int value) {
         if (root == null)
             return new AVLNode(value);
 
         if (value < root.value)
-            root.leftChild = insert(root.leftChild,value);
+            root.leftChild = insert(root.leftChild, value);
         else
-            root.rightChild = insert(root.rightChild,value);
+            root.rightChild = insert(root.rightChild, value);
+
+        root.height = Math.max(height(root.leftChild), height(root.rightChild)) + 1;
+
+        if (isLeftHeavy(root))
+            System.out.println(root.value + " Current node left heavy.");
+        else if (isRightHeavy(root))
+            System.out.println(root.value + " Current node right heavy.");
 
         return root;
+    }
+
+    private boolean isLeftHeavy(AVLNode node) {
+        return balanceFactor(node) > 1;
+    }
+
+    private boolean isRightHeavy(AVLNode node) {
+        return balanceFactor(node) < -1;
+    }
+
+    private int balanceFactor(AVLNode node) {
+        return (node == null) ? 0 : height(node.leftChild) - height(node.rightChild);
+    }
+
+    private int height(AVLNode node) {
+        return node == null ? -1 : node.height;
     }
 
 }
