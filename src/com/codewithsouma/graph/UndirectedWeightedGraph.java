31284f4be465e611d71067f974dc78f665faef38
CodeWithSouma <souma.hitech@gmail.com>
2021-07-05 23:27:03 +0530

    undirected weighted graph v2 complete

--- src/com/codewithsouma/graph/UndirectedWeightedGraph.java
+++ src/com/codewithsouma/graph/UndirectedWeightedGraph.java
@@ -8,9 +8,18 @@ import java.util.List;
 public class UndirectedWeightedGraph {
     private class Node {
         private String label;
+        private List<Edge> edges = new ArrayList<>();
 
         private Node(String label) {
             this.label = label;
+        }
+
+        public void addEdges(Node to, int weight){
+            edges.add(new Edge(this, to, weight));
+        }
+
+        public List<Edge> getEdges(){
+            return edges;
         }
 
         @Override
@@ -37,26 +46,37 @@ public class UndirectedWeightedGraph {
     }
 
     private HashMap<String, Node> nodes = new HashMap<>();
-    private HashMap<Node, List<Edge>> adjacencyList = new HashMap<>();
+    //private HashMap<Node, List<Edge>> adjacencyList = new HashMap<>();
 
     public void addNode(String label) {
         var node = new Node(label);
         nodes.putIfAbsent(label, node);
-        adjacencyList.putIfAbsent(node, new ArrayList<Edge>());
+
+        //old implementation
+        //adjacencyList.putIfAbsent(node, new ArrayList<Edge>());
     }
 
     public void addEdge(String from, String to, int weight) {
-        var toNode = nodes.get(to);
         var fromNode = nodes.get(from);
+        var toNode = nodes.get(to);
         if (toNode == null || fromNode == null) throw new IllegalArgumentException();
 
-       adjacencyList.get(fromNode).add(new Edge(fromNode, toNode, weight));
-       adjacencyList.get(toNode).add(new Edge(toNode, fromNode, weight));
+        //old implementation
+       //adjacencyList.get(fromNode).add(new Edge(fromNode, toNode, weight));
+       //adjacencyList.get(toNode).add(new Edge(toNode, fromNode, weight));
+
+        fromNode.addEdges(toNode,weight);
+        toNode.addEdges(fromNode,weight);
     }
 
     public void print() {
-        for (var source : adjacencyList.keySet())
-            System.out.println(source + " is connected with: " + adjacencyList.get(source));
+       /* for (var source : adjacencyList.keySet())
+            System.out.println(source + " is connected with: " + adjacencyList.get(source));*/
+
+        for (var node: nodes.values()){
+            System.out.println(node + " is connected with: "+node.getEdges());
+        }
+
     }
 
 }
